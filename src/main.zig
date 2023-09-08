013decb39c4e3dc50d0826134e87451eaa3f10c4
Tyler Calder <calder-ty@protonmail.com>
2023-09-08 08:25:56 -0600

    test: playing with ncurses

--- src/main.zig
+++ src/main.zig
@@ -4,26 +4,51 @@ const c = @cImport({
     @cInclude("locale.h");
 });
 
-const text = "Bi Cuimhneach";
+const CString = [:0]u8;
 
-pub fn main() void {
-    const x: c_int = c.LC_ALL;
-    const y = "";
+const MAX_READ_BYTES = 1 << 20;
 
-    _ = c.setlocale(x, y);
+const NcursesError = error{ SetUpError, RefreshFailure };
 
-    //     initscr
-    _ = c.initscr();
+pub fn main() !void {
+    try setup_ncurses();
+
+    const text = try read_input(std.heap.c_allocator);
+    defer std.heap.c_allocator.free(text);
+
+    _ = c.addstr(text.*);
     _ = c.refresh();
-    var i: usize = 0;
-    while (i < text.len) : (i += 1) {
-        _ = c.addch(text[i]);
-    }
-    _ = c.refresh();
-    std.time.sleep(1_000_000_000);
+
+    std.time.sleep(1_500_000_000);
     const errcode = c.endwin();
     switch (errcode) {
         0 => return,
         else => std.debug.print("An Error occured, error code {d}", .{errcode}),
     }
+}
+
+fn setup_ncurses() NcursesError!void {
+    const x: c_int = c.LC_ALL;
+    const y = "";
+
+    const ret_code = c.setlocale(x, y);
+    if (ret_code == null) {
+        std.debug.print("Unable to set locale\n", .{});
+        return NcursesError.SetUpError;
+    }
+
+    _ = c.initscr();
+
+    if (c.refresh() != 0) {
+        std.debug.print("Failure refreshing screen\n", .{});
+        return NcursesError.RefreshFailure;
+    }
+}
+
+/// Read the Input, returning the contents of the file.
+/// Allocated
+fn read_input(allocator: std.mem.Allocator) ![]u8 {
+    const file = std.io.getStdIn();
+    const content = try file.reader().readAllAlloc(allocator, MAX_READ_BYTES);
+    return content;
 }
