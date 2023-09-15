13f688fe9f9a6acd1645f43953b525fc296a12e0
Tyler Calder <calder-ty@protonmail.com>
2023-09-15 14:55:04 -0600

    feat: Test parser

--- src/vtparse.zig
+++ src/vtparse.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const pt = @import("./vtparse_table.zig");
 
 const MAX_INTERMEDIATE_CHARS = 2;
@@ -18,33 +19,45 @@ fn state(table_entry: u8) ?pt.ParserState {
     return @enumFromInt(val);
 }
 
-pub const ParserCallback = *const fn (vtparse: *VTParser, action: pt.Action, char: u8) void;
+pub const ParserCallback = *const fn (state: *const ParserData, to_action: pt.Action, char: u8) void;
 
-pub const VTParser = struct {
+pub const ParserData = struct {
     state: pt.ParserState,
-    cb: ParserCallback,
     intermediate_chars: [MAX_INTERMEDIATE_CHARS + 1]u8,
     num_intermediate_chars: u8,
     ignore_flagged: u8,
     params: [16]u32,
     num_params: u32,
+
+    fn init() ParserData {
+        return ParserData{
+            .state = pt.ParserState.GROUND,
+            .num_intermediate_chars = 0,
+            .ignore_flagged = 0,
+            .num_params = 0,
+            .intermediate_chars = undefined,
+            .params = undefined,
+        };
+    }
+};
+
+pub const VTParser = struct {
+    data: ParserData,
+    cb: ParserCallback,
     //     void*              user_data;
 
     pub fn init(cb: ParserCallback) VTParser {
         return VTParser{
-            .state = pt.ParserState.GROUND,
+            .data = ParserData.init(),
             .cb = cb,
-            .num_intermediate_chars = 0,
-            .ignore_flagged = 0,
-            .num_params = 0,
         };
     }
 
     pub fn parse(self: *VTParser, data: []const u8, len: usize) void {
-        var i = 0;
+        var i: usize = 0;
         while (i < len) : (i += 1) {
             const ch = data[i];
-            const state_change = pt.parse_table[@intFromEnum(self.state) - 1][ch];
+            const state_change = pt.parse_table[@intFromEnum(self.data.state) - 1][ch];
             self.doStateChange(state_change, ch);
         }
     }
@@ -55,50 +68,50 @@ pub const VTParser = struct {
         // we hand to our client for processing
 
         switch (in_action) {
-            pt.Action.PRINT, pt.Action.EXECUTE, pt.Action.HOOK, pt.Action.PUT, pt.Action.OSC_START, pt.Action.OSC_PUT, pt.Action.OSC_END, pt.Action.UNHOOK, pt.Action.CSI_DISPATCH, pt.Action.ESC_DISPATCH => self.cb(self, in_action, ch),
+            pt.Action.PRINT, pt.Action.EXECUTE, pt.Action.HOOK, pt.Action.PUT, pt.Action.OSC_START, pt.Action.OSC_PUT, pt.Action.OSC_END, pt.Action.UNHOOK, pt.Action.CSI_DISPATCH, pt.Action.ESC_DISPATCH => self.cb(&self.data, in_action, ch),
 
             pt.Action.IGNORE => {},
 
             pt.Action.COLLECT => {
                 // Append the character to the intermediate params
-                if (self.num_intermediate_chars + 1 > MAX_INTERMEDIATE_CHARS) {
+                if (self.data.num_intermediate_chars + 1 > MAX_INTERMEDIATE_CHARS) {
                     // Is this a bool?
-                    self.ignore_flagged = 1;
+                    self.data.ignore_flagged = 1;
                 } else {
-                    self.intermediate_chars[self.num_intermediate_chars] = ch;
-                    self.num_intermediate_chars += 1;
+                    self.data.intermediate_chars[self.data.num_intermediate_chars] = ch;
+                    self.data.num_intermediate_chars += 1;
                 }
             },
 
             pt.Action.PARAM => {
                 // process the param character
                 if (ch == ';') {
-                    self.num_params += 1;
-                    self.params[self.num_params - 1] = 0;
+                    self.data.num_params += 1;
+                    self.data.params[self.data.num_params - 1] = 0;
                 } else {
                     // the character is a digit
                     var current_param: usize = undefined;
 
-                    if (self.num_params == 0) {
-                        self.num_params = 1;
-                        self.params[0] = 0;
+                    if (self.data.num_params == 0) {
+                        self.data.num_params = 1;
+                        self.data.params[0] = 0;
                     }
 
-                    current_param = self.num_params - 1;
-                    self.params[current_param] *= 10;
-                    self.params[current_param] += (ch - '0');
+                    current_param = self.data.num_params - 1;
+                    self.data.params[current_param] *= 10;
+                    self.data.params[current_param] += (ch - '0');
                 }
             },
 
             pt.Action.CLEAR => {
-                self.num_intermediate_chars = 0;
-                self.num_params = 0;
-                self.ignore_flagged = 0;
+                self.data.num_intermediate_chars = 0;
+                self.data.num_params = 0;
+                self.data.ignore_flagged = 0;
             },
         }
     }
 
-    fn doStateChange(self: *VTParser, change: pt.ParserState, ch: u8) void {
+    fn doStateChange(self: *VTParser, change: u8, ch: u8) void {
         // A state change is an action and/or a new state to transition to
 
         const new_state = state(change);
@@ -120,19 +133,19 @@ pub const VTParser = struct {
             //
             // But because the table needs to be represented in simple bytes, the Null
             // byte was picked to represent that.
-            const exit_action = pt.exit_actions[@intFromEnum(self.state) - 1];
+            const exit_action = pt.exit_actions[@intFromEnum(self.data.state) - 1];
             const entry_action = pt.entry_actions[@intFromEnum(ns) - 1];
 
             if (exit_action) |act| {
-                self.doAction(self, act, 0);
+                self.doAction(act, 0);
             }
             if (in_action) |act| {
-                self.doAction(self, act, ch);
+                self.doAction(act, ch);
             }
             if (entry_action) |act| {
-                self.doAction(self, act, 0);
+                self.doAction(act, 0);
             }
-            self.state = ns;
+            self.data.state = ns;
         } else {
             if (in_action) |act| {
                 self.doAction(act, ch);
@@ -141,17 +154,13 @@ pub const VTParser = struct {
     }
 };
 
-test "VTParser" {
-    var i = 0;
-    const input = "abcd";
-
-    fn parserCallback(vtparse: *VTParser, action: pt.Action, char: u8) void {
-        std.debug.assert(input[i] == char);
-        i++;
-    }
-
-    parser = VTParser.init(parserCallback);
-    parser.parse(input, input.len());
-
+fn testCallback(parser_state: *const ParserData, to_action: pt.Action, ch: u8) void {
+    std.debug.print("Recieved action: {?}, State: {?} parser, char: {?}\n", .{ to_action, parser_state, ch });
 }
 
+test "VTParser" {
+    const input = [_]u8{ 27, '[', '2', '2', 'm', 'a', 'b', 'c', 'd' };
+
+    var parser = VTParser.init(testCallback);
+    parser.parse(&input, input.len);
+}
